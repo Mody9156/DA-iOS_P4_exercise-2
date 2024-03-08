b575ee1025d2191d449aa00df4298991d1989edb
Modibo KEITA <modykeita2@gmail.com>
2024-03-08 10:03:36 +0100

    création de la view ViewtypeGrid,iewtypeList et UserPickerView

---
+++ UserList/Sources/View/ViewtypeGrid.swift
@@ -0,0 +1,20 @@
+//
+//  ViewtypeGrid.swift
+//  UserList
+//
+//  Created by KEITA on 08/03/2024.
+//
+
+import SwiftUI
+
+struct ViewtypeGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewtypeGrid_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewtypeGrid()
+    }
+}
---
+++ UserList/Sources/View/ViewtypeList.swift
@@ -0,0 +1,20 @@
+//
+//  ViewtypeList.swift
+//  UserList
+//
+//  Created by KEITA on 08/03/2024.
+//
+
+import SwiftUI
+
+struct ViewtypeList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewtypeList_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewtypeList()
+    }
+}
